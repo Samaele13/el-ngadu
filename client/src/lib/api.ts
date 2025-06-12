250a383e8409c52ff70c5429841c183e974053a7
Raihan Akbar Gunawan <raihanakbarg28@gmail.com>
2025-06-12 18:45:52 +0700

    Initial Commit

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://el-ngadu.test/api',
+  baseURL: "http://el-ngadu.test/api",
   withCredentials: true,
 });
 
--- server/public/index.php
+++ server/public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(204);
+  exit();
+}
+
 header('Content-Type: application/json');
 
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
